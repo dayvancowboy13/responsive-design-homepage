366374e3726453b43872d7ff34d9f663770cbcdd
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-08-29 10:10:44 -0700

    Fix issue with webpack and css
    When both building dist and using the dev server, webpack didn't load
    the css file correctly. I didn't want to import it into the javascript,
    so did some Googling and found the solution: just add the css file to
    the entry list.

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  'entry': './src/index.js',
+  'entry': ['./src/index.js', './src/style.css'],
   'plugins': [
     new HtmlWebpackPlugin({
       'template': './src/template.html',
