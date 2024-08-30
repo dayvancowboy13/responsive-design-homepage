ef0359e60a4013bc815ab29fe2688dd9f17808cc
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-08-29 17:10:24 -0700

    Work on header file and various other things
    Bad commit :( Haven't been adding files and making commits

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import portraitImg from './images/350x350.png';
+
+const headerDiv = document.querySelector('.img-container');
+
+const img1 = new Image();
+img1.src = portraitImg;
+
+
+headerDiv.prepend(img1);
