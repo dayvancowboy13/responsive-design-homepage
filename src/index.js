42038f3476148c807f0bec471b2322eb1901f09c
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-08-31 07:29:40 -0700

    Add functionality to insert images into the cards
    Also removed a stray index.js in the project root dir

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import portraitImg from './images/350x350.png';
+import ProjectScrnSht from './images/project-screenshot.png';
 
 const headerDiv = document.querySelector('.img-container');
 
@@ -8,3 +9,22 @@ img1.src = portraitImg;
 
 
 headerDiv.prepend(img1);
+
+const workCards = document.querySelectorAll('.work-card');
+console.log(workCards);
+
+
+const scrnsht = new Image();
+scrnsht.src = ProjectScrnSht;
+
+console.log(scrnsht);
+
+for (let i = 0; i < workCards.length; i++) {
+
+    console.log(workCards[i].querySelector('.img-container'));
+
+    workCards[i].querySelector('.img-container').innerHTML = scrnsht.outerHTML;
+
+    console.log(workCards[i].querySelector('.img-container'));
+
+}
