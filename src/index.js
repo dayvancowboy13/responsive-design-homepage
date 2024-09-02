c2fde1711d11fab099a6e9a7ac8bbd85d23bdbe2
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-09-02 07:48:25 -0700

    Add new portrait image and tweak header

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import portraitImg from './images/350x350.png';
+import portraitImg from './images/portrait.png';
 import ProjectScrnSht from './images/project-screenshot.png';
 
 const headerDiv = document.querySelector('.img-container');
@@ -11,20 +11,14 @@ img1.src = portraitImg;
 headerDiv.prepend(img1);
 
 const workCards = document.querySelectorAll('.work-card');
-console.log(workCards);
 
 
 const scrnsht = new Image();
 scrnsht.src = ProjectScrnSht;
 
-console.log(scrnsht);
-
 for (let i = 0; i < workCards.length; i++) {
-
-    console.log(workCards[i].querySelector('.img-container'));
 
     workCards[i].querySelector('.img-container').innerHTML = scrnsht.outerHTML;
 
-    console.log(workCards[i].querySelector('.img-container'));
 
 }
