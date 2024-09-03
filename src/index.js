32df0ce9f27b716229a5fff74fb2fa51ab68d0f5
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-09-03 07:03:40 -0700

    Add photo for footer
    Had to do the JS code to add it. Also some changes to styling.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import './style.css';
 import portraitImg from './images/portrait.png';
 import ProjectScrnSht from './images/project-screenshot.png';
+import bottomPhoto from './images/bottom-photo.png';
 
 const headerDiv = document.querySelector('.img-container');
-
 const img1 = new Image();
 img1.src = portraitImg;
-
-
 headerDiv.prepend(img1);
+
+const footerImgContainer = document.querySelector('footer').querySelector('.img-container');
+const img2 = new Image();
+img2.src = bottomPhoto;
+footerImgContainer.append(img2);
 
 const workCards = document.querySelectorAll('.work-card');
 
