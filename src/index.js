6e48b1c59b6a84603fe818d5b1a825477082df0b
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-09-09 08:55:47 -0700

    Add real images of me and add responsive images
    Replaced placeholder images in header and footer with pictures of me..
    Header image scales properly, and footer image properly switches between
    wider and then portrait image when going form desktop to tablet width

--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import './style.css';
 import portraitImg from './images/portrait.png';
 import ProjectScrnSht from './images/project-screenshot.png';
 import bottomPhoto from './images/bottom-photo.png';
+import mePortrait from './images/me_portrait.jpg';
+import meWide from './images/me-650x400.png';
+import meTop from './images/me-450x500.png';
+
 
 const headerDiv = document.querySelector('.img-container');
 const img1 = new Image();
-img1.src = portraitImg;
+img1.src = meTop;
 headerDiv.prepend(img1);
 
 const footerImgContainer = document.querySelector('footer').querySelector('.img-container');
 const img2 = new Image();
 img2.src = bottomPhoto;
-footerImgContainer.append(img2);
+console.log(bottomPhoto);
+// footerImgContainer.append(img2);
+footerImgContainer.innerHTML = `
+<picture>
+    <source media="(max-width: 799px)" srcset="" />
+    <source media="(max-width: 1099px)" srcset="${mePortrait}" />
+
+    <img src="${meWide}">
+</picture>
+`;
 
 const workCards = document.querySelectorAll('.work-card');
 
